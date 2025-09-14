7651007f697f647c757798591ceede597a6312d6
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-09-14 11:39:40 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,5 @@
-number1 = int(10)
-number2 = int(5)
+number1 = 10
+number2 = 5
 
 addition = number1 + number2
 subtraction = number1 - number2
@@ -7,4 +7,5 @@ multiplication = number1 * number2
 
 print ("Addition of 10 and 5 is", addition,
          "\nSubtraction of 10 and 5 is", subtraction,
+
          "\nMultiplication of 10 and 5 is", multiplication)
