95f3119415febee21548f47e62a362a035890bd8
Betemariyam Abate <abatebetemariyam@gmail.com>
2025-09-14 11:36:37 +0300

    Add files via upload

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = int(10)
+number2 = int(5)
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print ("Addition of 10 and 5 is", addition,
+         "\nSubtraction of 10 and 5 is", subtraction,
+         "\nMultiplication of 10 and 5 is", multiplication)
